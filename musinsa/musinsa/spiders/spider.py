a46da30a6b5b30024e678db4d281ffe3edbd06f2
mieyhgnaj <jhijhi215@gmail.com>
2021-02-26 21:23:46 +0900

    separate selenium

--- musinsa/musinsa/spiders/spider.py
+++ musinsa/musinsa/spiders/spider.py
@@ -1,45 +1,61 @@
 
 import scrapy
 from musinsa.items import MusinsaItem
-# from selenium import webdriver
+from selenium import webdriver
+from fake_useragent import UserAgent
 
 
 class Spider(scrapy.Spider):
     name = "Musinsa"
     allow_domain = ["musinsa.com"]
     start_urls = ["https://search.musinsa.com/category/001003"]
-
+    custom_settings = {
+        'CONCURRENT_REQUESTS': 3,
+        'DOWNLOAD_DELAY': 6.0,
+        'AUTOTHROTTLE_ENABLED': True,
+        "DOWNLOADER_MIDDLEWARES": {
+            "scrapy.downloadermiddlewares.useragent.UserAgentMiddleware": None,
+            "scrapy_fake_useragent.middleware.RandomUserAgentMiddleware": 400,
+        }
+    }
+    
     def parse(self, response):
         links = response.xpath('//*[@id="searchList"]/li/div[contains(@class,"li_inner")]\
 /div[2]/p[2]/a/@href').extract()
         for link in links:
             yield scrapy.Request(link, callback=self.parse_content)
             
-    def parse_content(self, response):
+    def parse_content(self, response):     
         item = MusinsaItem()
-        title = response.xpath('/html/head/title/text()')[0].extract().split(' -')[0]
-        item["title"] = title.split(') ')[1]
-        item["brand"] = response.xpath('//*[@id="product_order_info"]/div[1]/ul/li[1]/p[2]/strong/a/text()')[0].extract()
-        try: # 세일가 있는 상품
-            item["o_price"] = response.xpath('//*[@id="goods_price"]/del/text()')[0].extract().strip()
-        except: # 판매가만 있는 상품
-            item["o_price"] = response.xpath('//*[@id="goods_price"]/text()')[0].extract().strip()
-        try:
-            item["s_price"] = response.xpath('//*[@id="sale_price"]/text()')[0].extract().strip()
-        except:
-            item["s_price"] = item["o_price"]
-        try:
-            item["kw"] = response.xpath('//*[@id="product_order_info"]/div[1]/ul/li[7]/p/a/text()').extract()
-        except:
-            item["kw"] = ''
-        item["img_link"] = "https:" + response.xpath('//*[@id="detail_bigimg"]/div[1]/img/@src')[0].extract()
-        item["link"] = response.url
-        item["item_id"] = item["link"].split('/')[-1]
+        options = webdriver.ChromeOptions()
+        options.add_argument("headless")
+        options.add_argument("user-agent={}".format(UserAgent().Chrome))
+        driver = webdriver.Chrome(options=options)        
+        driver.get(response.url)       
         
-        size_kind = response.xpath('//*[@id="size_table"]/tbody/tr/th/text()').extract()
-        item["size_details"] = {}
-        for idx in range(len(size_kind)-1):
-            sd = response.xpath(f'//*[@id="size_table"]/tbody/tr[{idx+3}]/*/text()').extract()
-            item["size_details"][sd[0]] = sd[1:]
-         
+        try:
+            item["most_age"] = driver.find_elements_by_xpath('//*[@id="graph_summary_area"]/strong[1]')[0].text
+        except:
+            item["most_age"] = 0
+        try:
+            item["most_sex"] = driver.find_elements_by_xpath('//*[@id="graph_summary_area"]/span[1]')[0].text
+        except:
+            item["most_sex"] = 0
+
+        option1_ls = driver.find_elements_by_xpath('//*[@id="option1"]/option[not(@value="")]')
+        option1 = [option.get_attribute("value") for option in option1_ls]
+        driver.find_element_by_xpath('//*[@id="option1"]/option[not(@value="")]').click()
+        option2_ls = driver.find_elements_by_xpath('//*[@id="option2"]/option[not(@value="")]')
+        option2 = [option.get_attribute("value") for option in option2_ls]
+
+        for_size = ['90', '95', 'S', 'M', 'L', 'LARGE', '1', 'M_쭈리']
+        if any(x in for_size for x in option1):
+            item["sizes"] = option1
+            item["colors"] = option2
+        else:
+            item["sizes"] = option2
+            item["colors"] = option1
+
+        driver.quit()
+        
         yield item
