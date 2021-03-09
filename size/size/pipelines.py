1de1b95ee700dd01dd1c24d197eaef826ada30ea
mieyhgnaj <jhijhi215@gmail.com>
2021-03-09 09:10:36 +0000

    modified

--- size/size/pipelines.py
+++ size/size/pipelines.py
@@ -1,13 +1,15 @@
-# Define your item pipelines here
-#
-# Don't forget to add your pipeline to the ITEM_PIPELINES setting
-# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
-
-# useful for handling different item types with a single interface
+import pandas as pd
 from itemadapter import ItemAdapter
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker
 
+
+engine = create_engine("mysql://root:<#SECRET>@<#SECRET>/musinsa?charset=utf8")
 
 class SizePipeline:
     def process_item(self, item, spider):
+        df = pd.DataFrame([item])
+        df.to_sql('size', con=engine, if_exists='append', index=False)
+        engine.execute("SELECT * FROM size").fetchall()
         return item
